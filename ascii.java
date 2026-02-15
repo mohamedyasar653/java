f06a4bea410d5a9ff3608b94bc7464dd7c0e4ff8
mohamedyasar653 <mohamedyasar653@gmail.com>
2026-02-15 22:55:19 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ascii {
+    public static void main(String[] args)
+    {
+        Scanner y=new Scanner(System.in);
+        System.out.println("enter  numbers");
+        int a=y.nextInt();
+        int b=y.nextInt();
+        int c=y.nextInt();
+        int d=y.nextInt();
+        System.out.println((char)a);
+        System.out.println((char)b);
+        System.out.println((char)c);
+        System.out.println((char)d);
+
+
+    }
+    
+}
